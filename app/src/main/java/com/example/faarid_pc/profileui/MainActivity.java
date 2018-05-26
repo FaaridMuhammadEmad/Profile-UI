71171442cce868efe0410b1a875c28a1078df0ab
Faarid M Emaad <faarid.me@gmail.com>
2018-05-26 05:41:51 +0500

    First commit

---
+++ app/src/main/java/com/example/faarid_pc/profileui/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.faarid_pc.profileui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
